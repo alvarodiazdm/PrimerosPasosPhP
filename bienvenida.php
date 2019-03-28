cbf97b7af131819b6fd146a3ee4ca1131b9a6bbf
Alvaro Diaz del Mazo <alvarodiazdm@gmail.com>
2019-03-28 14:18:45 +0100

    Primer paso

---
+++ bienvenida.php
@@ -0,0 +1,11 @@
+<!doctype html>
+<html>
+<head>
+<title>Bienvenida</title>
+<meta charset="utf-8">
+</head>
+<body>
+<p>¡Hola, <?php echo $_REQUEST['nombre']?>!</p>
+<p>Bienvenido a la curso de PHP,</p>
+</body>
+</html>
---
+++ index.php
@@ -0,0 +1,9 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: alvar
+ * Date: 28/03/2019
+ * Time: 13:44
+ */
+echo phpinfo();
+?>
